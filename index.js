429a3db170c5c87b3ba5dafbc03ca98a934611d0
Chrispine Oburu <chrizoburu65@gmail.com>
2024-03-14 14:50:06 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.innerHTML = 'Oburu is the champion';
+document.querySelector('main#main').appendChild(newHeader);
+
+document.querySelector('main#main').remove();
